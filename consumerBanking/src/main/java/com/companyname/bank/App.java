6c580a303839db4bc5522b9bd30504fc087a88cf
gangu17496 <92116578+gangu17496@users.noreply.github.com>
2021-11-04 01:18:30 +0530

    github push changes

--- consumerBanking/src/main/java/com/companyname/bank/App.java
+++ consumerBanking/src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! new word added code modified" );
+        System.out.println( "Hello World! new word added code modified by dev1" );
     }
 }
